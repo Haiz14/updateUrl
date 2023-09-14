54a1e94bdabf324a4930162cb23af50a3ba9070e
Haiz14 <haiz.gigs@gmail.com>
2023-09-14 12:59:21 +0530

    initial update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script >
+  import Paper, { Title, Content } from '@smui/paper';
+</script>
+<div class="paper-container">
+
+  <Paper color="primary" variant="outlined" class="mdc-theme--primary">
+    <Title>Outlined Primary Paper with Primary Text</Title>
+    <Content>
+      Outlined primary paper with primary color text. For the times when you
+      need to draw attention.
+    </Content>
+  </Paper>
+</div>
+
+
